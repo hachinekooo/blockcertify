77066931892c1eb276ec1447f39b320ad4c75b00
wangwenpeng <hachineko@yeah.net>
2025-09-03 21:48:03 +0800

    feat(core): 调整目录结构，新增注解

---
+++ blockcertify-core/src/main/java/com/github/blockcertify/annotation/BlockchainCertify.java
@@ -0,0 +1,25 @@
+package com.github.blockcertify.annotation;
+
+public @interface BlockchainCertify {
+
+    /*
+    * 业务类型
+    * */
+    String bizType();
+
+    /*
+     * 描述信息
+     **/
+    String description() default "";
+
+   /*
+   * 是否开启异步存证
+   * */
+   boolean async() default true;
+
+   /*
+   * 是否支持重试
+   * */
+   boolean retryable() default true;
+
+}
