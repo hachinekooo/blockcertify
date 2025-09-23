058dd76ff89286313236bf4e269b51ee339d8745
wangwenpeng <hachineko@yeah.net>
2025-09-23 23:21:58 +0800

    feat(core): 修改枚举值，补充定义

--- blockcertify-core/src/main/java/com/github/blockcertify/support/enums/CertifyRecordStatusEnum.java
+++ blockcertify-core/src/main/java/com/github/blockcertify/support/enums/CertifyRecordStatusEnum.java
@@ -4,18 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum CertifyRecordStatusEnum {
-      INIT("初始化"),           // 刚保存到数据库
-      PROCESSING("处理中"),      // 正在调用SDK
-      SUBMITTED("已提交"),      // 已提交到区块链，等待打包
-      CONFIRMING("确认中"),     // 区块链确认中
-      SUCCESS("成功"),          // 存证成功
-      FAILED("失败"),            // 存证失败
-      DISABLED("功能未启用");    // 新增状态
+      INIT("INIT", "初始化"),           // 刚保存到数据库
+      PROCESSING("PROCESSING", "处理中"),      // 正在调用SDK
+      SUBMITTED("SUBMITTED", "已提交"),      // 已提交到区块链，等待打包
+      CONFIRMING("CONFIRMING", "确认中"),     // 区块链确认中
+      SUCCESS("SUCCESS", "成功"),          // 存证成功
+      FAILED("FAILED", "失败"),            // 存证失败
+      DISABLED("DISABLED", "功能未启用");    // 禁用
     ;
 
     private final String status;
+    private final String desc;
 
-    CertifyRecordStatusEnum(String status) {
+    CertifyRecordStatusEnum(String status, String desc) {
         this.status = status;
+        this.desc = desc;
     }
 }
